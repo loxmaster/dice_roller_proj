c9eb656095dff197533e425f4c5c60d8eb55349e
Sharonm1 <sharonm1@rafael.co.il>
2022-12-14 13:24:37 +0200

    test

--- src/classes/Unit.js
+++
@@ -1,6 +0,0 @@
-class Unit {
-  name;
-  price;
-  faction = [];
-  constructor() {}
-}
---
+++ src/classes/Unit.ts
@@ -0,0 +1,6 @@
+class Unit {
+  name: string;
+  price: number;
+  faction: string[] = [];
+  constructor() {}
+}
