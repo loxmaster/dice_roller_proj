a8e9e53420f21e1fbc89a34126d32eb73ce89008
Sharonm1 <sharonm1@rafael.co.il>
2022-12-27 14:14:42 +0200

    added some stuff

--- src/classes/Scroll.ts
+++ src/classes/Scroll.ts
@@ -1,13 +1,16 @@
 //father of all the Warscrolls, each has name,price and faction
-class Scroll {
-  name = null;
-  price = 0;
-  faction = [];
-  role = undefined;
-  constructor(name, price, faction, role) {
-    this.name = name;
-    this.price = price;
-    this.faction = faction;
-    if (role) this.role = role;
-  }
+interface Scroll {
+  name: string;
+  price: number;
+  faction: string[];
+  role: string[];
+  models: number;
+  unique: boolean;
+
+  getName();
+  getPrice();
+  getFaction();
+  getRole();
+  getModels();
+  isUnique();
 }
--- src/classes/Unit.ts
+++ src/classes/Unit.ts
@@ -1,6 +1,24 @@
-class Unit {
-  name: string;
-  price: number;
-  faction: string[] = [];
-  constructor() {}
+class Unit implements Scroll {
+  //generic stats
+  private name: string;
+  private price: number;
+  private faction: string[] = [];
+  private role: string[];
+  //maybe transform into stats array ?
+  private attacks: number;
+  private toHit: number;
+  private toWound: number;
+  private rend: number;
+
+  //defensive stats
+  private toSave: number;
+  private toWard: number = 7;
+
+  //other stats
+  private move: number;
+  private models: number;
+  private wounds: number = 1;
+  private unique: boolean = false;
+
+  //constructor(name: string, price: number, faction: string[], role: string[]) {}
 }
